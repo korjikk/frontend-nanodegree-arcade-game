9ee4fdc15e94c20d04a94c6cac71b72e65210e76
Sergiu Catan <sergiu.n.catan@gmail.com>
2018-07-29 17:40:39 +0300

    Basic implementation of the game

--- README.md
+++ README.md
@@ -1,6 +1,14 @@
 frontend-nanodegree-arcade-game
 ===============================
+## Introduction
+Hello!
+This is a simple arcade game, where you need to move your character from the start to the "water" tiles.
 
-Students should use this [rubric](https://review.udacity.com/#!/projects/2696458597/rubric) for self-checking their submission. Make sure the functions you write are **object-oriented** - either class functions (like Player and Enemy) or class prototype functions such as Enemy.prototype.checkCollisions, and that the keyword 'this' is used appropriately within your class and class prototype functions to refer to the object the function is called upon. Also be sure that the **readme.md** file is updated with your instructions on both how to 1. Run and 2. Play your arcade game.
+## How to run the game:
+Just run the index.html file.
 
-For detailed instructions on how to get started, check out this [guide](https://docs.google.com/document/d/1v01aScPjSWCCWQLIpFqvg3-vXLH2e8_SZQKC8jNO0Dc/pub?embedded=true).
+## How to play the game:
+* You can control your character by using the arrow keys.
+* You should avoid the contact with the bugs. The goal is to reach the "water" tiles.
+* You can see your score in the upper part of the window. 
+* You can also reset the score and the player position by pressing the "Reset" button.
--- js/app.js
+++ js/app.js
@@ -1,40 +1,176 @@
-// Enemies our player must avoid
-var Enemy = function() {
-    // Variables applied to each of our instances go here,
-    // we've provided one for you to get started
+//updates the score bar
+function updateScoreBar(gamesLost, gamesWon) {
+    if (gamesLost != undefined) {
+        document.querySelector('.gamesLostValue').textContent = gamesLost;
+    }
+    if (gamesWon != undefined) {
+        document.querySelector('.gamesWonValue').textContent = gamesWon;
+    }
+}
 
+//resets the score and put the player to it's initial positon
+function resetGame() {
+    gamesLost = 0;
+    gamesWon = 0;
+    updateScoreBar(gamesLost, gamesWon);
+    player.setInitialCoordinates();
+}
+
+// Enemies our player must avoid
+var Enemy = function () {
     // The image/sprite for our enemies, this uses
     // a helper we've provided to easily load images
     this.sprite = 'images/enemy-bug.png';
+    //initial coordonates
+    this.x = 0;
+    this.y = 0;
+    //initial speed
+    this.speed = 0;
 };
 
 // Update the enemy's position, required method for game
 // Parameter: dt, a time delta between ticks
-Enemy.prototype.update = function(dt) {
+Enemy.prototype.update = function (dt) {
+    if (this.y === player.y && Math.floor((this.x + 50) / 100) === player.x / 100) {
+        player.setInitialCoordinates();
+        gamesLost++;
+        updateScoreBar(gamesLost);
+    }
+    //if the enemy object has crossed the screen
+    if (this.x > 505) {
+        //reset the position when the object is out of view
+        this.x = -100;
+        //set a random y positon (random row)
+        this.setRandomRowCoordinate();
+        //set a new random speed
+        this.setRandomSpeed(100, 250);
+    }
+    // You should multiply any movement by the dt parameter
+    // which will ensure the game runs at the same speed for
+    // all computers.
+
+    //move the enemy object horizontaly
+    this.x += this.speed * dt;
+
+};
+
+// Draw the enemy on the screen, required method for game
+Enemy.prototype.render = function () {
+    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+};
+
+//sets the speed of the enemy object to a random value between the min and max arguments
+Enemy.prototype.setRandomSpeed = function (min, max) {
+    //sets speed to a random value within the interval
+    this.speed = Math.random() * (max - min) + min;
+};
+
+//puts the enemy object on a random 'concrete' row
+Enemy.prototype.setRandomRowCoordinate = function () {
+    //the array of possible Y values (to situate the "enemy" on one of the three concrete rows)
+    var possibleYValues = [60, 145, 230];
+    //sets Y to a random value from the possibleYValues array
+    this.y = possibleYValues[Math.floor(Math.random() * possibleYValues.length)];
+};
+
+// the Player class
+var Player = function () {
+    // The image/sprite for our player, this uses
+    // a helper we've provided to easily load images
+    this.sprite = 'images/char-boy.png';
+    this.x = 0;
+    this.y = 0;
+};
+
+
+Player.prototype.update = function (dt) {
     // You should multiply any movement by the dt parameter
     // which will ensure the game runs at the same speed for
     // all computers.
 };
 
+Player.prototype.setInitialCoordinates = function () {
+    player.y = 400;
+    player.x = 200;
+};
+
+//handles the movement of the player
+Player.prototype.handleInput = function (dt) {
+    var verticalMove = 85;
+    var horizontalMove = 100;
+
+    //check if the water tile is reached
+    if (dt === 'up' && this.y - verticalMove < 0) {
+        //resets the player coordinates;
+        player.setInitialCoordinates();
+        gamesWon++;
+        updateScoreBar(null, gamesWon);
+        return;
+    }
+    //check if the move would be out of bound
+    else if ((dt === 'down' && this.y + verticalMove > 400) ||
+        (dt === 'left' && this.x - horizontalMove < 0) || (dt === 'right' && this.x + horizontalMove > 400)
+    ) {
+        return;
+    }
+    else
+        switch (dt) {
+            case 'up': {
+                this.y -= verticalMove;
+                break;
+            }
+            case 'down': {
+                this.y += verticalMove;
+                break;
+            }
+            case 'left': {
+                this.x -= horizontalMove;
+
+                break;
+            }
+            case 'right': {
+                this.x += horizontalMove;
+
+                break;
+            }
+        }
+
+};
+
 // Draw the enemy on the screen, required method for game
-Enemy.prototype.render = function() {
+Player.prototype.render = function () {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 };
 
-// Now write your own player class
-// This class requires an update(), render() and
-// a handleInput() method.
+//instantiating the enemies objects and pushing them to the allEnemies array;
+var allEnemies = [];
+//create 4 enemy objects
+for (var i = 0; i < 4; i++) {
+    var newEnemy = new Enemy();
+    //randomly set an Y value, so the enemy object will be on a random 'concrete' row
+    newEnemy.setRandomRowCoordinate();
+    //set a new random speed
+    newEnemy.setRandomSpeed(100, 300);
+    //place it off screen
+    newEnemy.x = -100;
 
+    allEnemies.push(newEnemy);
+}
 
-// Now instantiate your objects.
-// Place all enemy objects in an array called allEnemies
-// Place the player object in a variable called player
+//instantiate the player object
+var player = new Player();
+//set the staring coordinates
+player.setInitialCoordinates();
 
+//global variables to store the game state
+var gamesWon = 0;
+var gamesLost = 0;
+updateScoreBar(gamesWon, gamesLost);
 
 
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method. You don't need to modify this.
-document.addEventListener('keyup', function(e) {
+document.addEventListener('keyup', function (e) {
     var allowedKeys = {
         37: 'left',
         38: 'up',
